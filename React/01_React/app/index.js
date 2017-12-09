7880243fb36561542da991813acb159165bb7747
Nikolai <rus.nick@mail.ru>
2017-12-09 22:30:51 +0300

    Add basic React scaffolding

---
+++ React/01_React/app/index.js
@@ -0,0 +1,7 @@
+import css from './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ React/Readme.md
@@ -0,0 +1,6 @@
+# React Scaffolding
+
+1. 01_React
+
+Base React Configuration. Dependencies: `webpack`, `webpack-dev-server`, `html-webpack-plugin`, `css-loader`, `babel-preset-{es2015, react, stage-0}`.
+ 
